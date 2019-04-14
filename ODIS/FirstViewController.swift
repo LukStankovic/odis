2ca9d267d25fe2e8be53995c7ab5a6b6d8c66a99
lukasstankovic <intelcoreI5>
2019-04-14 14:58:03 +0200

    Initial Commit

---
+++ ODIS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ODIS
+//
+//  Created by Lukáš Stankovič on 14/04/2019.
+//  Copyright © 2019 Lukáš Stankovič. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ODIS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ODIS
+//
+//  Created by Lukáš Stankovič on 14/04/2019.
+//  Copyright © 2019 Lukáš Stankovič. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
